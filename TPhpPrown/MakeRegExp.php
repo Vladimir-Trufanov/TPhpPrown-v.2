0d5222d969215c506e36c08c3dcb5a72d6449736
Vladimir-Trufanov <tve@karelia.ru>
2020-04-07 16:08:11 +0300

    Тестирование MakeRegExp.php

--- TPhpPrown/MakeRegExp.php
+++ TPhpPrown/MakeRegExp.php
@@ -19,12 +19,13 @@
 // MakeRegExp может использоваться для настройки функции Findes перед встраиваем
 // её в код сценария PHP.
 
+require_once "iniConstMem.php";
 require_once "iniErrMessage.php";
 require_once "MakeUserError.php";
 
 // Синтаксис:
 //
-//   MakeRegExp($pattern,$text,&$matches=null,$isTrass=true);
+//   MakeRegExp($pattern,$text,&$matches=null,$isTrass=mriIsDeprecated);
 
 // Параметры:
 //
@@ -32,25 +33,26 @@ require_once "MakeUserError.php";
 //   $text    - текст, который должен быть обработан регулярным выражением;
 //   $matches - массив найденных фрагментов и позиций их начала после работы
 //      регулярного выражения (параметр по ссылке);
-//   $isTrass=true (по умолчанию), если следует выполнить трассировку найденных
-//      соответствий регулярному выражению.
-//   $isPrintr - флаг управления трассировкой, используется в сочетании с флагом
-//      $isTrass. true (по умолчанию). Если  если следует выполнить трассировку найденных
-//      соответствий регулярному выражению.
+//   $isTrass - режим трассировки найденных соответствий регулярному выражению:
+//
+//   mriStandTracing - трассировка результатов стандартным выводом;
+//   mriInstallTrace - установленная трассировка MakeRegExp
+//   mriTracingBlock - трассировка заблокирована
+//   mriIsDeprecated - разбор и сообщение устаревшего использования (по умолчанию)
 
-// Возвращаемое значение: 
+/// Возвращаемое значение: 
 //
 //   $Result  - количество найденных соответствий регулярному выражению.
 //      $Result=0, если соответствий не найдено. 
 
 // Зарегистрированные ошибки/исключения:
 //   
-// 1 - "Устарела выборка подстроки регулярным выражением".
+//   FetchStrObsolete - "Устарела выборка подстроки регулярным выражением".
 
 // ****************************************************************************
 // *  Выполнить функцию preg_match_all, при необходимости, отттрассировать ее *
 // ****************************************************************************
-function MakeRegExp($pattern,$text,&$imatches=null,$isTrass=true,$isPrintr=true)
+function MakeRegExp($pattern,$text,&$imatches=null,$isTrass=mriIsDeprecated)
 {
    $Prefix='TPhpPrown';
    
@@ -61,16 +63,26 @@ function MakeRegExp($pattern,$text,&$imatches=null,$isTrass=true,$isPrintr=true)
    //        C:\Webservers\kwinflat-ru\www\TPHPPROWN\regx.php on line 20
    
    // Готовим массив результатов
-   if ($imatches===null) $imatches=array(); 
+   if ($imatches===null) $matches=array(); 
+   else $matches=$imatches;
    // Выполняем регулярное выражение и получаем результаты поиска
-   $Result=preg_match_all($pattern,$text,$imatches,PREG_OFFSET_CAPTURE);
-   print_r($imatches); 
-
-   // При трассировке показываем текст, шаблон поиска 
-   if ($isTrass)
+   $Result=preg_match_all($pattern,$text,$matches,PREG_OFFSET_CAPTURE);
+   // Выводим исходные данные трассировки
+   if (($isTrass==mriStandTracing)||($isTrass==mriInstallTrace))
    {
-      echo '<br>'.'$text: '.$text;
-      echo '<br>'.'$pattern: '.$pattern;
+      echo '<br>'.'$text: <strong>'.$text.'</strong>';
+      echo '<br>'.'$pattern: <strong>'.$pattern.'</strong><br>';
+   }
+   // Выполняем трассировку результатов стандартным выводом
+   if ($isTrass==mriStandTracing)
+   {
+      echo 'mriStandTracing:<br>';
+      print_r($matches); 
+   }
+   // Выполняем установленную трассировка MakeRegExp
+   if ($isTrass==mriInstallTrace)
+   {
+      echo 'mriInstallTrace:';
       // Показываем нулевой результат, если поиск неудачный  
       if ($Result==0)
       {
@@ -80,30 +92,26 @@ function MakeRegExp($pattern,$text,&$imatches=null,$isTrass=true,$isPrintr=true)
       // что найдено и позиция
       else 
       {
-         /*
-         for ($i=0; $i<count($imatches); $i++)
+         for ($i=0; $i<count($matches); $i++)
 		   {
-            $findes=$imatches[$i];    
+            $findes=$matches[$i];    
             for ($j=0; $j<count($findes); $j++)
             {
-               echo '<br>$findes['.$j.'] = '.
-               $findes[$j][0].' Point = '.
-               $findes[$j][1];  
+               echo '<br>'.
+               '$matches['.$i.']['.$j.'][0] = <strong>'.$matches[$i][$j][0].'</strong> ... '.
+               '$matches['.$i.']['.$j.'][1] = <strong>'.$matches[$i][$j][1].'</strong>';
             }
          }
-         */
       }
-      echo '<br>';
-      
-      
-      
    }
    // Если трассировка не нужна, то выдаем сообщение:
    // "Устарела выборка подстроки регулярным выражением"
-   else
-   {
-      prown\MakeUserError(FetchStrObsolete,'TPhpPrown',rvsCurrentPos,E_USER_DEPRECATED);
-   }   
+   //else
+   //{
+   //   prown\MakeUserError(FetchStrObsolete,'TPhpPrown',rvsCurrentPos,E_USER_DEPRECATED);
+   //}
+   // Возвращаем результат регулярного выражения
+   $imatches=$matches;
    return $Result;
 }
 // ********************************************************* MakeRegExp.php ***
--- TPhpPrownTests/MakeRegExp_test.php
+++ TPhpPrownTests/MakeRegExp_test.php
@@ -2,7 +2,7 @@
 
 // PHP7/HTML5, EDGE/CHROME                          *** MakeRegExp_test.php ***
 // ****************************************************************************
-// * TPhpPrown      Тест функции MakeRegExp - Отработать регулярное выражение *
+// * TPhpPrown      Тест функции MakeRegExp - отработать регулярное выражение *
 // *                                       на тексте и оттрассировать разбор. *
 // *                       Рекомендуется использовать только для трассировки. *
 // *                                                                          *
@@ -12,30 +12,36 @@
 
 class test_MakeRegExp extends UnitTestCase 
 {
-   // Здесь все должно хорошо найтись в своих позициях
-   function test_MakeRegExp()
+   // Внимание: Имя функции/метода теста не должно совпадать с именем класса
+
+   function test_MakeRegExp_All()
    {
       MakeTitle("MakeRegExp",'');
-      $string='Это строка для проверки функции MakeRegExp';
-      $preg='/Это/u';
-      $prefix='MakeRegExp("'.$preg.'","'.$string.'"); ';
-      $Result=prown\MakeRegExp($preg,$string,$matches);
-      //$this->assertEqual('Это',$Result);
-      //MakeTestMessage($prefix,'Подстрока '.'"Это"'.' найдена в строке',80);
 
-      $string='Строкой этой проверяется функция MakeRegExp для поиска "это"';
-      $preg='/это/u';
-      $prefix='MakeRegExp("'.$preg.'","'.$string.'"); ';
-      $Result=prown\MakeRegExp($preg,$string,$matches);
-      //$this->assertEqual('это',$Result);
-      //MakeTestMessage($prefix,'Подстрока '.'"Это"'.' найдена в строке',80);
+      $string="В этой строке ищется 'это' функцией MakeRegExp";
+      $preg="/это/u";
+      $prefix='<br>MakeRegExp("'.$preg.'","'.$string.'"); ';
+      $Result=prown\MakeRegExp($preg,$string,$matches,mriStandTracing);
+      $this->assertEqual('это',$matches[0][1][0]);
+      $this->assertEqual(39,$matches[0][1][1]);
+      MakeTestMessage($prefix,'Подстроки '.'"это"'.' найдены, стандартная трассировка',80);
 
-      $string='Строкой этой проверяется функция MakeRegExp для поиска "это"';
-      $preg='/эти/u';
-      $prefix='MakeRegExp("'.$preg.'","'.$string.'"); ';
-      $Result=prown\MakeRegExp($preg,$string,$matches);
-      //$this->assertEqual('это',$Result);
-      //MakeTestMessage($prefix,'Подстрока '.'"Это"'.' найдена в строке',80);
+      $string="В этой строке ищется 'эти' функцией MakeRegExp";
+      $preg="/эти/u";
+      $prefix='<br>MakeRegExp("'.$preg.'","'.$string.'"); ';
+      $Result=prown\MakeRegExp($preg,$string,$matches,mriInstallTrace);
+      $this->assertEqual('эти',$matches[0][0][0]);
+      $this->assertNotEqual(3,$matches[0][0][1]);
+      $this->assertEqual(39,$matches[0][0][1]);
+      MakeTestMessage($prefix,'Подстрока '.'"эти"'.' найдена, установленная трассировка MakeRegExp',80);
+
+      $string="В этой строке не ищется 'это' функцией MakeRegExp";
+      $preg="/этот/u";
+      $prefix='<br>MakeRegExp("'.$preg.'","'.$string.'"); ';
+      $Result=prown\MakeRegExp($preg,$string,$matches,mriStandTracing);
+      $this->assertEqual('это',$matches[0][1][0]);
+      $this->assertEqual(39,$matches[0][1][1]);
+      MakeTestMessage($prefix,'Подстроки '.'"это"'.' найдены, стандартная трассировка',80);
    }
 }
 // **************************************************** MakeRegExp_test.php ***
